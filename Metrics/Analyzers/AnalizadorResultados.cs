95ea32feb24e1ca8c0113d1008b5d34b52a2472d
alexxdiazz-20 <r.alexdiaz.20@gmail.com>
2025-08-13 22:07:29 -0400

    feat(metrics): implementa colector de metricas y analizador

---
+++ Metrics/Analyzers/AnalizadorResultados.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrigadasEmergenciaRD.Metrics.Analyzers
+{
+    // Calculos de speedup y eficiencia
+    public static class AnalizadorResultados
+    {
+        public static double Speedup(double tSecuencial, double tParalela)
+        {
+            return tParalela <= 0 ? 0 : tSecuencial / tParalela;
+        }
+
+        public static double Eficiencia(double speedup, int p)
+        {
+            return p <= 0 ? 0 : speedup / p;
+        }
+
+        public static (double speedup, double eficiencia) Comparar(double tSec, double tPar, int p)
+        {
+            var s = Speedup(tSec, tPar);
+            return (s, Eficiencia(s, p));
+        }
+    }
+}
---
+++ Metrics/Collectors/MetricasRendimiento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using BrigadasEmergenciaRD.Metrics.Interfaces;
+using BrigadasEmergenciaRD.Metrics.Models;
+
+namespace BrigadasEmergenciaRD.Metrics.Collectors
+{
+    // Colector basico de metricas
+    public sealed class MetricasRendimiento : IMetricas
+    {
+        private readonly MetricBuffer _buffer = new();
+
+        public void BeginOp() { }
+
+        public void EndOp(long elapsedTicks, bool success)
+        {
+            _buffer.LatenciasTicks.Enqueue(elapsedTicks);
+            if (success) Interlocked.Increment(ref _buffer.Exitos);
+            else Interlocked.Increment(ref _buffer.Fallos);
+        }
+
+        public void AddBytes(long bytes) { Interlocked.Add(ref _buffer.Bytes, bytes); }
+
+        public MetricSnapshot Snapshot()
+        {
+            return MetricBuffer.ToSnapshot("default", _buffer, (int)(_buffer.Exitos + _buffer.Fallos), Environment.ProcessorCount);
+        }
+
+        public void Reset()
+        {
+            while (_buffer.LatenciasTicks.TryDequeue(out _)) { }
+            _buffer.Exitos = 0;
+            _buffer.Fallos = 0;
+            _buffer.Bytes = 0;
+        }
+
+        public void Start() { _buffer.StartTimers(); }
+        public (double cpuSeg, double memMb, double durSeg) Stop() { return _buffer.StopTimers(); }
+    }
+}
